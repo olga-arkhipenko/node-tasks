b9c4d80673504ca82fb2a1abd5a5b9d1970ed0b8
olga-arkhipenko <26512909+olga-arkhipenko@users.noreply.github.com>
2020-10-12 02:50:40 +0300

    Parse CLI arguments (#4)
    * delete cli.js; add services.js
    
    * add cli arguments parsing

--- dad-jokes-cli/index.js
+++ dad-jokes-cli/index.js
@@ -1,1 +1,14 @@
-console.log('index file');
+#!/usr/bin/env node
+const { searchJoke, showLeaderboard } = require('./services');
+
+const SEARCH_MODE = '--searchTerm';
+const LEADERBOARD_MODE = '--leaderboard';
+
+const [ , , mode, searchTerm] = process.argv;
+
+if (mode === LEADERBOARD_MODE) {
+  showLeaderboard();
+} else if (mode === SEARCH_MODE && searchTerm) {
+  searchJoke(searchTerm);
+} else throw new Error('No arguments provided');
+
---
+++ dad-jokes-cli/services.js
@@ -0,0 +1,8 @@
+const searchJoke = term => console.log('searching by', term);
+
+const showLeaderboard = () => console.log('leaderborad');
+
+module.exports = {
+  searchJoke,
+  showLeaderboard
+};
