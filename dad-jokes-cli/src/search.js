8fd585ec536e36761c6d983119a2b0adafbbbcd7
Olga Arkhipenko <orangeorca909@gmail.com>
2020-10-14 01:52:38 +0300

    add random joke searching

--- dad-jokes-cli/src/search.js
+++ dad-jokes-cli/src/search.js
@@ -1,13 +1,28 @@
 const { get } = require('./request');
+const { randomInRange } = require('./utils');
 
 const SEARCH_URL = 'https://icanhazdadjoke.com/search';
+const PAGE_LIMIT = 30;
 
 const searchJoke = async term => {
-  const query = new URLSearchParams({ term }).toString();
+  const query = new URLSearchParams({ term, limit: PAGE_LIMIT }).toString();
 
   try {
-    const joke = await get(`${SEARCH_URL}?${query}`);
-    console.log(joke);
+    const {
+      results: firstPageJokes,
+      total_jokes: totalJokesAmount
+    } = await get(`${SEARCH_URL}?${query}`);
+
+    const randomJokeNumber = randomInRange(1, totalJokesAmount);
+
+    if(randomJokeNumber <= PAGE_LIMIT) {
+      console.log(totalJokesAmount, randomJokeNumber, firstPageJokes[randomJokeNumber - 1]);
+    } else {
+      const query = new URLSearchParams({ term, page: randomJokeNumber, limit: 1 }).toString();
+      const { results } = await get(`${SEARCH_URL}?${query}`);
+      console.log(totalJokesAmount, randomJokeNumber, results[0]);
+    }
+
   } catch (e) {
     console.error(e);
   }
