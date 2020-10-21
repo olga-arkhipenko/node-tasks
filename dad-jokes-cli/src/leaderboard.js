6554c61b620d387bf5f286b5af07fd8626840b6d
Olga Arkhipenko <orangeorca909@gmail.com>
2020-10-21 20:14:14 +0300

    implement leaderboard

--- dad-jokes-cli/src/leaderboard.js
+++ dad-jokes-cli/src/leaderboard.js
@@ -1,3 +1,15 @@
-const showLeaderboard = () => console.log('leaderboard');
+const { readJokes } = require('./file');
+
+const showLeaderboard = () => {
+  const jokes = readJokes();
+  const sortedJokes = Object.entries(jokes).sort((jokeFirst, jokeSecond) => jokeSecond[1].counter - jokeFirst[1].counter);
+
+  if(!sortedJokes.length) {
+    console.log('There are no jokes yet');
+    return;
+  }
+
+  console.log(sortedJokes[0][1].joke);
+};
 
 module.exports = { showLeaderboard };
