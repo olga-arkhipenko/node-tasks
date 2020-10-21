c6696002fdf7131363269708e0a5a47129c9647b
Olga Arkhipenko <orangeorca909@gmail.com>
2020-10-21 03:39:32 +0300

    add error handling

--- dad-jokes-cli/src/search.js
+++ dad-jokes-cli/src/search.js
@@ -1,4 +1,5 @@
 const { get } = require('./request');
+const { updateFile } = require('./file');
 
 const SEARCH_URL = 'https://icanhazdadjoke.com/search';
 const PAGE_LIMIT = 30;
@@ -15,15 +16,25 @@ const searchJoke = async term => {
       total_jokes: totalJokesAmount
     } = await get(`${SEARCH_URL}?${query}`);
 
+    if(!totalJokesAmount) {
+      console.log('No jokes were found for that search term');
+      return;
+    }
+
     const randomJokeNumber = randomInRange(1, totalJokesAmount);
 
+    let randomJoke;
+
     if(randomJokeNumber <= PAGE_LIMIT) {
-      console.log(totalJokesAmount, randomJokeNumber, firstPageJokes[randomJokeNumber - 1]);
+      randomJoke = firstPageJokes[randomJokeNumber - 1];
     } else {
       const query = new URLSearchParams({ term, page: randomJokeNumber, limit: 1 }).toString();
       const { results } = await get(`${SEARCH_URL}?${query}`);
-      console.log(totalJokesAmount, randomJokeNumber, results[0]);
+      randomJoke = results[0];
     }
+
+    console.log(randomJoke.joke);
+    updateFile(randomJoke);
 
   } catch (e) {
     console.error(e);
